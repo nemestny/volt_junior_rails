bbb9c782d31a34b2ff1ca2594cabc56ceac10ee8
Aleksandr Noskov <nemestny@politeh.ru>
2019-02-04 21:04:57 +0500

    Try post create with rspec

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -9,15 +9,28 @@ module Api
       end
 
       def create
-        render json: {
-          error: 'create'
-        }
+        # p params
+        # p permit_params
+        post = Post.new permit_params
+        post.user = current_user
+        if post.valid?
+          post.save
+          render json: post.to_json
+        else
+          render json: {errors: post.errors.messages}
+        end
       end
 
       def show
         render json: {
           error: params #test comment
         }
+      end
+
+      private
+
+      def permit_params
+        params.permit(:title, :body, :published_at)
       end
     end
   end
--- spec/controllers/api/v1/posts_controller_spec.rb
+++ spec/controllers/api/v1/posts_controller_spec.rb
@@ -14,16 +14,32 @@ RSpec.describe Api::V1::PostsController, type: :controller do
   end
 
   before (:all) do
-    user = create(:user)
-    @token = JsonWebToken.encode(user_id: user.id)
+    @user = create(:user)
+    @token = JsonWebToken.encode(user_id: @user.id)
   end
+
+  let(:correct_create_post) { {params: {title: 'example title', body: 'example body', published_at: Time.now }}}
+  let(:incorrect_create_post) { {params: {title: 'example title'}}}
+
 
   describe 'post create' do
     context 'when user authorised' do
       it 'should return error message' do
         request.headers["Authorization"] = @token
-        resp = get :index
-        expect(JSON.parse(resp.body)).to eq({})
+        resp = post :create, incorrect_create_post
+        puts resp.body
+        expect(JSON.parse(resp.body)['title']).to eq('example title')
+        expect(JSON.parse(resp.body)['body']).to eq('example body')
+        expect(JSON.parse(resp.body)['author_nickname']).to eq(@user.nickname)
+      end
+
+      it 'should return error message' do
+        request.headers["Authorization"] = @token
+        resp = post :create, correct_create_post
+        puts resp.body
+        expect(JSON.parse(resp.body)['title']).to eq('example title')
+        expect(JSON.parse(resp.body)['body']).to eq('example body')
+        expect(JSON.parse(resp.body)['author_nickname']).to eq(@user.nickname)
       end
     end
   end
