8155b8efb45cd927dcb377385e9f6e0d5418ee2c
Aleksandr Noskov <nemestny@politeh.ru>
2019-02-10 20:29:05 +0500

    Take params for report

--- app/jobs/reports_generate_job.rb
+++ app/jobs/reports_generate_job.rb
@@ -1,8 +1,12 @@
 class ReportsGenerateJob < ApplicationJob
   queue_as :default
 
-  def perform(*args)
-    puts args
+  def perform(*params)
+    start_date = params[0][:start_date] || Time.now
+    end_date = params[0][:end_date] || Time.now
+    email = params[0][:email] || 'nemestny@politeh.ru'
+
+
     # Do something later
   end
 end
