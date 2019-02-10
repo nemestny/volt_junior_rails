0180b6d59567cb6df83b037542dc364b287cefd4
Aleksandr Noskov <nemestny@politeh.ru>
2019-02-10 11:47:09 +0500

    Add layout

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,7 @@
 class Api::V1::UsersController < ApplicationController
   include ActionController::RequestForgeryProtection
+  include ActionView::Layouts
+  layout 'application'
 
   # skip_before_action :authenticate_request
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
@@ -8,8 +10,9 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    @current_user.avatar.attach(permit_params[:avatar])
+    @current_user.avatar.attach(permit_params[:avatar]) if permit_params[:avatar]
     p @current_user.avatar.attached?
+    redirect_to root_path
   end
 
   private
